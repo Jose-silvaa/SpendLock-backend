75d0077e59a0966350bc81b8e6cae4b977ad0a54
Jose-silvaa <jvitorfacanha@gmail.com>
2025-10-30 23:32:26 -0300

    feat: visitors table

---
+++ Dtos/Visitor/CreateVistorResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace spendlock_backend.Dtos.Visitor;
+
+public record CreateVistorResponseDto(Guid Id, string Name, string Email, DateTime CreatedAt)
+{
+    public static CreateVistorResponseDto FromVisitor(Models.Email.Visitor visitor) =>
+        new(visitor.Id, visitor.Name, visitor.Email, visitor.CreatedAt);
+}
---
+++ Models/Email/Visitor.cs
@@ -0,0 +1,21 @@
+﻿using spendlock_backend.Interfaces;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace spendlock_backend.Models.Email;
+
+[Table("visitors")]
+public class Visitor : BaseModel
+{
+    [PrimaryKey("id", false)]
+    public Guid Id { get; set; }
+    
+    [Column("name")]
+    public string Name { get; set; }
+    
+    [Column("email")]
+    public string Email { get; set; }
+    
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
